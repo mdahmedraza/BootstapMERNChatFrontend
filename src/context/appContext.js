e60de655055ae08e3c3db4e0a1b26db6035da876
Ahamed Raza <ahamedraza2233@gmail.com>
2024-03-31 11:34:52 +0530

    Update appContext.js
    changing url

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,7 +1,8 @@
 import {io} from 'socket.io-client';
 import React from 'react';
 
-const SOCKET_URL = 'http://localhost:5001';
+// const SOCKET_URL = 'http://localhost:5001';
+const SOCKET_URL = 'https://bootstrapmernchat.onrender.com';
 
 export const socket = io(SOCKET_URL);
 // app context..
